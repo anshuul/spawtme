9d8c61e9e59f52c7df41a2d77516133bc88d5cbf
anshuul <anshultated1999@gmail.com>
2021-06-21 13:51:04 +0530

    fixed url params

--- src/App.js
+++ src/App.js
@@ -4,11 +4,9 @@ import Navbar from "./components/Navbar/Navbar";
 import {
   LANDING_PAGE_ROUTE,
   LINKS_PAGE_ROUTE,
-  ERROR_PAGE_ROUTE,
 } from "./routes";
 import Landing from "./pages/Landing/Landing";
 import Links from "./pages/Links/Links";
-import Error from "./pages/Error/Error";
 
 class App extends Component {
   render() {
@@ -17,9 +15,8 @@ class App extends Component {
         <BrowserRouter>
           <Navbar />
           <Switch>
-            <Route path={LANDING_PAGE_ROUTE} component={Landing} />
+            <Route exact path={LANDING_PAGE_ROUTE} component={Landing} />
             <Route exact path={LINKS_PAGE_ROUTE} component={Links} />
-            <Route exact path={ERROR_PAGE_ROUTE} component={Error} />
             <Route exact path="/">
               <Redirect to={LANDING_PAGE_ROUTE} />
             </Route>
--- src/routes.js
+++ src/routes.js
@@ -1,3 +1,4 @@
-export const LANDING_PAGE_ROUTE = "/landing";
-export const LINKS_PAGE_ROUTE = "/links/:id";
-export const ERROR_PAGE_ROUTE = "/links";
+export const LANDING_PAGE_ROUTE = "/";
+export const LINKS_PAGE_ROUTE = "/:id";
+/* export const ERROR_PAGE_ROUTE = "/links/error";
+ */
