4d3b599aabbc42a020740d745a505b98e82f9b06
anshuul <anshultated1999@gmail.com>
2021-06-21 20:33:59 +0530

    added amterialize css

--- src/pages/Landing/Landing.js
+++ src/pages/Landing/Landing.js
@@ -15,38 +15,152 @@ class Landing extends Component {
   render() {
     return (
       <Aux>
-        <div className="custom-div-1">
-          <div className="image-div-1">
+
+        <div class="row  ">
+          <div class="col s12 m12 l6 customImageDiv">
+
             <img
               id="customImage"
               src={Image1}
               alt="image1"
               style={{ cursor: "default" }}
             />
+
           </div>
-          <div className="text-div-1">
-            <h1>
-              Discover People <br /> By Social
+          <div class="col s12 m12 l6">
+            <div className="text-div-1">
+              <h1>
+                Discover People <br /> By Social
             </h1>
-            <p>
-              Lorem ipsum dolor sit amet consectetur <br /> adipisicing elit.
+              <p>
+                Lorem ipsum dolor sit amet consectetur <br /> adipisicing elit.
               Tempora labore ea nobis ullam <br /> quasi maiores impedit
               tempore, animi ad <br /> delectus eius aut,
             </p>
-            <a
-              href="https://play.google.com/store/apps/details?id=com.social.friends.games.multiplayer.spawtme"
-              target="blank"
-            >
-              <img
-                src={GooglePlay}
-                alt="googleplay"
-                onClick="https://play.google.com/store/apps/details?id=com.social.friends.games.multiplayer.spawtme"
-              />
-            </a>
+              <a
+                href="https://play.google.com/store/apps/details?id=com.social.friends.games.multiplayer.spawtme"
+                target="blank"
+              >
+                <img
+                  src={GooglePlay}
+                  alt="googleplay"
+                  onClick="https://play.google.com/store/apps/details?id=com.social.friends.games.multiplayer.spawtme"
+                />
+              </a>
+            </div>
           </div>
         </div>
-        <div className="custom-div-2">
-          <div className="text-div-2">
+
+
+
+
+        <div class="row customDiv2 ">
+          <div class="col s12 m12 l6 customTextDiv">
+            <div className="text-div-2">
+              <h1>
+                Discover People <br /> By Social
+            </h1>
+              <p>
+                Lorem ipsum dolor sit amet consectetur <br /> adipisicing elit.
+              Tempora labore ea nobis ullam <br /> quasi maiores impedit
+              tempore, animi ad <br /> delectus eius aut,
+            </p>
+
+            </div>
+          </div>
+          <div class="col s12 m12 l6 customImageDiv">
+
+            <img
+              id="customImage"
+              src={Image2}
+              alt="image1"
+              style={{ cursor: "default" }}
+            />
+          </div>
+        </div>
+
+
+
+
+
+
+
+
+
+
+        <div class="row  customDiv3">
+          <div class="col s12 m12 l6 customImageDiv">
+
+            <img
+              id="customImage"
+              src={Image3}
+              alt="image1"
+              style={{ cursor: "default" }}
+            />
+
+          </div>
+          <div class="col s12 m12 l6">
+            <div className="text-div-1">
+              <h1>
+                Discover People <br /> By Social
+            </h1>
+              <p>
+                Lorem ipsum dolor sit amet consectetur <br /> adipisicing elit.
+              Tempora labore ea nobis ullam <br /> quasi maiores impedit
+              tempore, animi ad <br /> delectus eius aut,
+            </p>
+            </div>
+          </div>
+        </div>
+
+
+
+        <div class="row customDiv4 ">
+          <div class="col s12 m12 l6 customTextDiv">
+            <div className="text-div-2">
+              <h1>
+                Discover People <br /> By Social
+            </h1>
+              <p>
+                Lorem ipsum dolor sit amet consectetur <br /> adipisicing elit.
+              Tempora labore ea nobis ullam <br /> quasi maiores impedit
+              tempore, animi ad <br /> delectus eius aut,
+            </p>
+            </div>
+          </div>
+          <div class="col s12 m12 l6 customImageDiv">
+
+            <img
+              id="customImage"
+              src={Image4}
+              alt="image1"
+              style={{ cursor: "default" }}
+            />
+
+          </div>
+        </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        {/* 
+        <div className="row customDiv2">
+          <div className="col s12 m12 l6 customTextDiv2">
             <h1>
               Discover People <br /> By Social
             </h1>
@@ -61,15 +175,17 @@ class Landing extends Component {
               target="blank"
             ></a>
           </div>
-          <div className="image-div-2">
+          <div className="col s12 m12 l6 customImageDiv">
             <img
-              id="customImage"
+
               src={Image2}
               alt="image2"
               style={{ cursor: "default" }}
             />
           </div>
-        </div>
+        </div> */}
+
+        {/* 
         <div className="custom-div-3">
           <div className="image-div-1">
             <img
@@ -93,8 +209,8 @@ class Landing extends Component {
               target="blank"
             ></a>
           </div>
-        </div>
-        <div className="custom-div-4">
+        </div> */}
+        {/*         <div className="custom-div-4">
           <div className="text-div-2">
             <h1>
               Discover People <br /> By Social
@@ -118,11 +234,11 @@ class Landing extends Component {
               style={{ cursor: "default" }}
             />
           </div>
-        </div>
-        <div className="custom-div-5">
+        </div> */}
+        <div className="row center-align">
           <p>@ {new Date().getFullYear()} Spawt.me</p>
         </div>
-      </Aux>
+      </Aux >
     );
   }
 }
